14948a8d140f862b7c8988efcaea95e70d8573c0
tollispa <112690202+tollispa@users.noreply.github.com>
2023-05-24 20:49:10 +0200

    sabotage

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET users listing. */
-const users = [
-  "TP", "PS", "DT", "VK"
-]
-router.get('/', function(req, res, next) {
-  res.send(users);
+router.get('/', (req, res) => {
+  res.json('TP, PS, DT, VK');
 });
-router.get('/sabotage', (req, res) => {
-  res.json('TP, PS, DT, VK.');
-});
+
 module.exports = router;
+
+
+router.get('/sabotage', (req, res) => {
+  res.json('TP, PS, DT, VK');
+});

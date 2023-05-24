155615fe1410c639360dbfc9950c4f2cd32193e8
tollispa <112690202+tollispa@users.noreply.github.com>
2023-05-24 21:11:11 +0200

    sabotage

---
+++ test/users.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+
+const app = require('../app');
+
+describe('GET /sabotage', function () {
+    it('trying to get /sabotage', function (done) {
+        request(app)
+            .get('/sabotage')
+            
+           
+            .expect(200, done);
+    });
+});
